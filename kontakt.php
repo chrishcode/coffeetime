3c24577b5f22560e5814b240f9daab571dd88f71
Christopher Wohlfarth <chris.wohlfarth@gmail.com>
2015-04-22 15:05:18 +0200

    kontakt sidan är klar

--- kontakt.php
+++ kontakt.php
@@ -18,9 +18,13 @@ END;
 //content
 $content = <<<END
 	<div id="content" class="container">
-		<p class="col-md-7 col-centered">
-			HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!HÄR SKA VA ETT FORMULÄR!!!
-		</p>
+		<form class="col-md-7 col-centered" action="sendmail.php" method="post">
+			<p>Om du har frågor så får du gärna skicka ett mail till oss via formuläret nedan.</p>
+			<label >NAMN</label><input class="form-control" type="text" name="name"><br>
+			<label>E-POST</label><input class="form-control" type="text" name="email"><br>
+			<label>MEDDELANDE</label><textarea class="form-control" name="msg" rows="7"></textarea>
+			<input class="btn btn-success" type="submit" value="SKICKA">
+		</form>
 	</div>
 END;
 
