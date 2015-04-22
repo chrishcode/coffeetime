3cc6db68b1c820b9014bfb58edec005198adec28
Petterromhagen <petter.romhagen@hotmail.com>
2015-04-22 13:37:04 +0200

    h1 tag

--- omoss.php
+++ omoss.php
@@ -18,6 +18,7 @@ END;
 //content
 $content = <<<END
 	<div id="content" class="container">
+			<h1> Petter </h1>
 		<p class="col-md-7 col-centered">
 			CoffeeTime är en nätbutik som startades under våren 2015. CoffeeTimes affärsidé är att erbjuda olika slags kaffemaskiner för att nå ut till en breddare kundkrets oavsett om det är i sitt eget hem, på ett café eller kontor. För att bäst kunna dra nytta av våra produkter så har vi handplockat ett urval av de bästa kaffemaskinerna som finns på marknaden idag. Krav vi kan ställa eftersom vi enbart sysslar med kaffemaskiner.
 			<p class="col-md-7 col-centered">CoffeeTime har ett stort utbud av kaffekokare från de ledande tillverkarna. Likaså presskvarnar, kapselmaskiner, mokkabryggare & espressomaskiner. De allra flesta produkterna finns i lager för omgående leverans.</p>
