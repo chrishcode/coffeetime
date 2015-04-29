824c20adfad7e29f69aecec574d75d4fcb5b5247
Christopher Wohlfarth <chris.wohlfarth@gmail.com>
2015-04-29 15:59:58 +0200

    lagt till product details och fler produkter som visas

--- productdetails.php
+++ productdetails.php
@@ -10,7 +10,7 @@ $products = ""; //tom variabel som tar emot databasinnehåll
 //databasfråga hämtar alla produkter och sorterar på kategori(get-variablen)
 $query = <<<END
 	SELECT * FROM produkt
-	WHERE Kategori = "{$_GET["category"]}"
+	WHERE ProduktID = "{$_GET["id"]}"
 END;
 
 //kör databasfrågan
@@ -20,14 +20,17 @@ $res = $conn->query($query);
 if($res->num_rows > 0) {
 	while($row = $res->fetch_object()) {
 		$productTitle = utf8_encode($row->ProduktNamn); //gör om produkt titel till utf-8 (åäö)
-		$products .= <<<END
-			<div class="product col-md-3">
-				<div class="crop"><img class="productimg" src="{$row->imgUrl}" alt="product"></div>
-				<div class="titlewrap"><h4>$productTitle</h4></div>
-				<h6>LAGERSTATUS<div class="{$row->Lagerstatus} img-circle"></div></h6>
-				<p class="price">{$row->Pris}kr</p>
-				<a class="btn btn-success pull-left" href="productdetails.php?id={$row->ProduktID}">LÄS MER</a>
-				<a class="btn btn-success pull-right" href="#">KÖP</a>
+		$productDesc = utf8_encode($row->Beskrivning); //gör om produktbeskrivning till utf-8 (åäö)
+		$product .= <<<END
+			<div class="detailproduct col-md-12">
+				<div class="detailcrop pull-left"><img class="productimg" src="{$row->imgUrl}" alt="product"></div>
+				<div class="detailinfo">
+					<h4 class="detailtitle pull-left">$productTitle</h4>
+					<p class="detaildesc">$productDesc</p>
+					<h6 class="detailstatus">LAGERSTATUS<div class="detail{$row->Lagerstatus} img-circle"></div></h6>
+					<p class="detailprice">{$row->Pris}kr</p>
+					<button class="btn btn-success detailbtn">KÖP</button>
+				</div>
 			</div>
 END;
 	}
@@ -41,7 +44,7 @@ $content = <<<END
 	<div id="content" class="container">
 		<div id="products" class="col-md-10 col-centered">
 			<h1 class="col-md-4 col-centered">$title</h1>
-			$products 
+			$product
 		</div>
 	</div>
 END;
